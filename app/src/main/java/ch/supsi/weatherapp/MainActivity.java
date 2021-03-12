4c4ed34bcab2a61f95254cc56ce7a8a6757e3221
Sebastian <sebsatian.nee@student.supsi.ch>
2021-03-12 10:48:42 +0100

    Added simple button that adds example place

--- app/src/main/java/ch/supsi/weatherapp/MainActivity.java
+++ app/src/main/java/ch/supsi/weatherapp/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import ch.supsi.weatherapp.controllers.UserLocationsHolder;
 import ch.supsi.weatherapp.model.Location;
@@ -12,11 +17,14 @@ import ch.supsi.weatherapp.model.Location;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    private FloatingActionButton newPlaceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         UserLocationsHolder.getInstance().getLocations().add(new Location("Strunz 14"));
         UserLocationsHolder.getInstance().getLocations().add(new Location("Strunz 2"));
@@ -28,5 +36,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
+        newPlaceButton = findViewById(R.id.newPlaceButton);
+        newPlaceButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                UserLocationsHolder.getInstance().getLocations().add(new Location("New Place 1"));
+                reloadPlaces();
+            }
+        });
+
+    }
+
+    private void reloadPlaces(){
+        LocationAdapter adapter = new LocationAdapter(UserLocationsHolder.getInstance().getLocations());
+
+        recyclerView = findViewById(R.id.locations_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
